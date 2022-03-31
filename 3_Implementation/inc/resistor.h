358bd791978dd1497245053e8e43ff81faa77ed1
BHARATH A J <93699185+bharathajckm@users.noreply.github.com>
2022-03-31 21:46:17 +0530

    Create resistor.h

---
+++ 3_Implementation/inc/resistor.h
@@ -0,0 +1,14 @@
+#ifndef __resistor_H__
+#define __resistor_H__
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<string.h>
+#include<Windows.h>
+
+void getColorCode();
+long getResistorValue();
+
+
+#endif
